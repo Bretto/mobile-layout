b13e2e44c693365eade18b209b74bb847d038268
Bretto <separ8@gmail.com>
2013-06-06 16:48:15 +1000

    add pages

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,13 @@
 'use strict';
 
-var app = angular.module('App', ['App.filters', 'App.services', 'App.directives', 'App.controllers', 'ui', 'ui.bootstrap']);
+var app = angular.module('App', ['App.filters', 'App.services', 'App.directives', 'App.controllers', 'ui', 'ui.bootstrap']).
+    config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+        $routeProvider.
+            when('/page/1', {templateUrl:'partials/page1.html', controller:'PageCtrl'}).
+            when('/page/2', {templateUrl:'partials/page2.html', controller:'PageCtrl'}).
+            when('/page/3', {templateUrl:'partials/page3.html', controller:'PageCtrl'}).
+            when('/page/4', {templateUrl:'partials/page4.html', controller:'PageCtrl'}).
+            when('/page/5', {templateUrl:'partials/page5.html', controller:'PageCtrl'}).
+            otherwise({redirectTo:'/page/1'});
+        $locationProvider.html5Mode(false);
+    }]);
--- public/js/services.js
+++ public/js/services.js
@@ -6,9 +6,7 @@ services.factory('DataModel', function ($http, $log, $rootScope, $routeParams, $
 
     var dataModel = {};
     dataModel.toggleViewOpen = true;
-    dataModel.pages
-
-
+    dataModel.pages = [{title:'Page: 1'}, {title:'Page: 2'}, {title:'Page: 3'}, {title:'Page: 4'}, {title:'Page: 5'}];
 
 
     return dataModel;
